595777f7d7e5f204ed780b01a4609d2219af9eb5
Sebastian Jablonski <sebaa1080@gmail.com>
2024-08-24 14:00:52 +0200

    Next.js frontend project init

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      Airbnb django next
+      <h2 className="text-airbnb">Lala</h2>
+      <h2 className="text-airbnbDark">Lala</h2>
+    </div>
+  );
+}
