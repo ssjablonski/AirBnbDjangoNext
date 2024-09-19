ae81c560bc8684aa949aeef52f00fe297a81ef47
Sebastian Jablonski <sebaa1080@gmail.com>
2024-09-19 15:08:25 +0200

    User model created

--- backend/django_backend/backend/settings.py
+++ backend/django_backend/backend/settings.py
@@ -16,6 +16,8 @@ DEBUG = bool(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 
+AUTH_USER_MODEL = 'useraccount.User'
+
 
 # Application definition
 
@@ -26,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'useraccount',
 ]
 
 MIDDLEWARE = [
